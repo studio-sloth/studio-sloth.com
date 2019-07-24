6ff66e5e9758cc46207e8225897191b7cb5cddeb
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2019-07-25 01:17:36 +0300

    using Svelte

---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<style>
+	section {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		flex-direction: column;
+	}
+</style>
+
+<section>
+	<h1>Studio Sloth</h1>
+	<p>Studio Sloth is a group of indie designers and developers trying out ideas
+		and projects, currently based in Istanbul, Turkiye.</p>
+</section>
